ba9cee70ee4cf2990b6c7fe376b71245551db1c5
bruentov <vengeance3007@gmail.com>
2026-05-04 10:22:44 +0500

    Create testing class with custom hashcode

---
+++ src/MyTestingClass.java
@@ -0,0 +1,29 @@
+public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash = 31 * hash + id;
+        if (name != null) {
+            for (int i = 0; i < name.length(); i++) {
+                hash = 31 * hash + name.charAt(i);
+            }
+        }
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) obj;
+        return id == that.id && name.equals(that.name);
+    }
+}
